37f8e6b8b62f836da780a3f700b646347b7bd434
axiiom <axiiom@u19s.home>
2019-12-12 13:09:56 -0500

    fixed issue with setup script

--- setup_quickscripts.sh
+++ setup_quickscripts.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-usrnms=( $@ )
-
 grpe=$(cat /etc/group | grep quickscripts)
 if [[ ${#grpe} -lt 1 ]]; then
 	echo -e "\e[34mCreating quickscripts"
 	sudo groupadd quickscripts
 fi
 
-for usr in $usrnms; do
-	ingrp=$(groups $usr | grep quickscripts)
-	if [[ ${#ingrp} -lt 1 ]]; then
-		echo "Adding $USER to quickscripts group"
-		sudo usermod -aG quickscripts $usr
-	fi
-done
-
 ingrp=$(groups | grep quickscripts)
 if [[ ${#ingrp} -lt 1 ]]; then
+	sudo usermod -aG quickscripts $USER
 	exec su -l $USER
 fi
 
@@ -26,6 +17,7 @@ echo -e "\e[34mCreating quickscripts directory and setting ownership"
 sudo mkdir /var/lib/quickscripts
 sudo chgrp quickscripts /var/lib/quickscripts
 sudo chmod g+rwx /var/lib/quickscripts
+echo -e "\e[34mDone ... make sure to run\n	sudo usermod -aG quickscripts <user>\n to give permissions to users"
 
 echo -e "\e[34mMoving myself and the rest of the scripts to the quickscripts directory"
 mv ${PWD}/* /var/lib/quickscripts
